093907d96e0d36e91021d97da205ca758ede794d
Mahesh Das <Mahesh.das2205@gmail.com>
2025-02-09 01:26:00 +0530

    First commit

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+    content: ["./src/**/*.{html,js,php}"], // Specify the files Tailwind should scan for classes
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1e90ff", // Example custom color
+        },
+      },
+    },
+    plugins: [], // Add plugins if needed
+  };
