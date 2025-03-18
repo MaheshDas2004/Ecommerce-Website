d21e151636af4ee823bb2ec6314336155050a546
Mahesh Das <Mahesh.das2205@gmail.com>
2025-03-18 12:12:15 +0530

    Add navbar component and update styles in output.css

---
+++ FRONTEND/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./output.css">
+</head>
+<body>
+    <div class=" bg-blue-950">
+        <nav class="flex justify-between items-center p-4">
+            <div class="text-white>">Logo</div>
+            
+            <div>
+                <input type="text" class="p-2 rounded-lg bg-white" placeholder="Search">
+            </div>
+            <div class="flex justify-between items-center">
+                <a href="index.php" class="text-white">Home</a>
+                <a href="login.php" class="text-white">Login</a>
+                <a href="signup.php" class="text-white">Signup</a>
+                <a href="mycart.php" class=" text-white">My Cart</a>
+            </div>
+        </nav>
+    </div>
+    
+</body>
+</html>
